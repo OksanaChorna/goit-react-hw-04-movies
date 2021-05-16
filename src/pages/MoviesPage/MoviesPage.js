5bf477e0c35bc7109ec8ceb8aaedfd17914dff2b
OksanaChorna <ksuchorna@gmail.com>
2021-05-17 00:04:12 +0300

    Пошук по запиту

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -1,28 +1,26 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Container from '../../components/Container';
 
 class MoviesPage extends Component {
   state = {
     query: '',
+    movies: [],
   };
-
-  async componentDidMount() {
-    const { query } = this.state;
-    const queryGetApi = await axios.get(
-      `https://api.themoviedb.org/3/${query}/movie?api_key=17f34524669c2658ba6f6a8fb0e96e0c`,
-    );
-    console.log(queryGetApi.data);
-
-    // this.setState({ movies: queryGetApi.data });
-  }
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(event);
-    // this.props.onSubmit(this.state.query);
-    this.setState({ query: event.currentTarget.value });
-    this.setState({ query: '' });
+    const { query } = this.state;
+    const queryGetApi = axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=17f34524669c2658ba6f6a8fb0e96e0c&query=${query}`,
+      )
+      .then(({ data }) => {
+        console.log(data.results);
+        this.setState({ movies: data.results });
+        this.setState({ query: '' });
+      });
   };
 
   handleChange = event => {
@@ -33,23 +31,33 @@ class MoviesPage extends Component {
     return (
       <>
         <section className="Searchbar">
-          <Container>
-            <form className="SearchForm" onSubmit={this.handleSubmit}>
-              <button type="submit" className="SearchForm-button">
-                <span className="SearchForm-button-label">Search</span>
-              </button>
+          <form className="SearchForm" onSubmit={this.handleSubmit}>
+            <button type="submit" className="SearchForm-button">
+              <span className="SearchForm-button-label">Search</span>
+            </button>
 
-              <input
-                className="SearchForm-input"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search movies"
-                value={this.state.query}
-                onChange={this.handleChange}
-              />
-            </form>
-          </Container>
+            <input
+              className="SearchForm-input"
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search movies"
+              value={this.state.query}
+              onChange={this.handleChange}
+            />
+          </form>
+          <div>
+            <ul>
+              {this.state.movies.map(movie => (
+                <li key={movie.id}>
+                  <Link to={`${this.props.match.url}/${movie.id}`}>
+                    {movie.title}
+                    {movie.original_name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </section>
       </>
     );
